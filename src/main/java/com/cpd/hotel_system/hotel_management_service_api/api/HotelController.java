dd324fd3ab21424cb8d45b89de590e4b248b429f
education-bootcamp <education.seekerscloud@gmail.com>
2025-08-02 10:58:46 +0530

    init

--- src/main/java/com/cpd/hotel_system/hotel_management_service_api/api/HotelController.java
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/api/HotelController.java
@@ -1,9 +1,31 @@
 package com.cpd.hotel_system.hotel_management_service_api.api;
 
+import com.cpd.hotel_system.hotel_management_service_api.dto.request.RequestHotelDto;
+import com.cpd.hotel_system.hotel_management_service_api.service.HotelService;
+import com.cpd.hotel_system.hotel_management_service_api.util.StandardResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/hotel-management/api/v1/hotels")
 public class HotelController {
+    private final HotelService hotelService;
+
+    @PostMapping("/user/create")
+    public ResponseEntity<StandardResponseDto> create(
+            @RequestBody RequestHotelDto dto) {
+        hotelService.create(dto);
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                    201, "Hotel Saved!",null
+                ),
+                HttpStatus.CREATED
+        );
+    }
 }
